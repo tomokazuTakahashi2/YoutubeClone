59b429521d2e8731e496eb852d335400eba56cab
tomokazutakahashi4 <takahashitomokazu4@gmail.com>
2020-10-07 23:44:47 +0900

    Initial Commit

---
+++ YoutubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeApp
+//
+//  Created by Raphael on 2020/10/07.
+//  Copyright © 2020 Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
